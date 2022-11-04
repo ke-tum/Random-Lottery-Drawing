c32fb0a3f58fecb3290f7de1f40f9e537f8bfbc7
ke-tum <111168404+ke-tum@users.noreply.github.com>
2022-11-04 15:37:02 -0700

    Added comments at beginning of program. Power draw no longer outputs as a list.

--- lottery.py
+++ lottery.py
@@ -1,14 +1,20 @@
+# Random-Lottery-Drawing
+# This program is inspired by Powerball
+
 import random
+
 
 reg_drawing = []
 power_drawing = []
+
 
 for i in range(5):
     if i not in reg_drawing:
         reg_drawing.append(random.randint(1, 69))
 
+
 for i in range(1):
     power_drawing.append(random.randint(1, 26))
 
-print(f'Your drawing is {reg_drawing}. Your Power Draw is {power_drawing}.')
+print(f'Your drawing is {reg_drawing}. Your Power Draw is {power_drawing[0]}.')
 print('Thanks for playing!')
